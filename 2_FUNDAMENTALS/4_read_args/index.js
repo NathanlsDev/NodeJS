16cf22fa2d312e6b1450d1b61ad0fb1f3293eb70
Nathan Lima <nathanls.dev@gmail.com>
2023-03-01 18:26:08 -0300

    02 - reading args

--- 2_FUNDAMENTALS/4_read_args/index.js
+++ 2_FUNDAMENTALS/4_read_args/index.js
@@ -1,11 +1,12 @@
 //nome
-console.log(process.argv)
+console.log(process.argv);
 
 // const args = process.argv.forEach((arg, index) => {
 //   console.log(arg, `index ${index}`)
 // })
 
-const arg = process.argv.slice(2)
-const name = arg[0].split('=')[1]
-const age = arg[1].split('=')[1]
-console.log(name, age)
+//need do a command 'node index.js name=yourName age=yourAge, to run correctly'
+const arg = process.argv.slice(2);
+const name = arg[0].split("=")[1]; 
+const age = arg[1].split("=")[1]; 
+console.log(`Your name is ${name}, and you is ${age} years old`);
